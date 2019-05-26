d2cc4b78d3f81d6c75eaa42e231257908e5b4cae
Moumita Saha <sahamoumita318@gmail.com>
2019-05-26 22:10:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+	render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div class="container">
+   <div class="row">
+   <div class="col-md-6">
+   <h1>mou kmn acho</h1>
+   </div>
+   <div class="row">
+   <div class="col-md-6">
+   <h1>neha</h1>
+   </div>
+   </div>
+   </div>
+   </div>
+
   );
+}
 }
 
 export default App;
